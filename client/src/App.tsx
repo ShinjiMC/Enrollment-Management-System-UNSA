02599ddf9166f4eb08a5d0826c35f2f4ab08b89f
alupoc <alupoc@unsa.edu.pe>
2024-07-14 02:30:41 -0500

    bakend: course repository

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,10 @@ import styles from "./sass/App.module.scss";
 import AuthPage from "./pages/AuthPage/AuthPage";
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
-import { useSelector } from "react-redux";
+import { useUser } from "./context/UserContext";
 
 function App(): JSX.Element {
-  const user = useSelector((state: any) => state.authReducer.authData);
+  const { user } = useUser();
 
   return (
     <div className={styles.app}>
@@ -17,7 +17,7 @@ function App(): JSX.Element {
             element={user ? <Navigate to="home" /> : <Navigate to="auth" />}
           />
           <Route path="/home" element={user ? <HomePage /> : <AuthPage />} />
-          <Route path="/auth" element={user ? <HomePage /> : <AuthPage />} />
+          <Route path="/auth" element={!user ? <AuthPage /> : <HomePage />} />
         </Routes>
       </div>
       <Footer />
