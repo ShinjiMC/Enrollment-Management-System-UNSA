52c659813d2bafab4009b5af20f34f40d58495b4
i-am-sergio <sergio15.dev@gmail.com>
2024-07-22 15:52:14 -0500

    backend: Update Program.cs

--- payments-microservice/src/Program.cs
+++ payments-microservice/src/Program.cs
@@ -10,11 +10,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-// Register Domain Services
-builder.Services.AddScoped<IPaymentProcessingService, PaymentProcessingService>();
-
 // Register application services
-builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.AddScoped<IElectronicBillService, ElectronicBillService>();
+
+// Register domain services
+builder.Services.AddScoped<IElectronicBillDomainService, ElectronicBillDomainService>();
 
 // Register repositories
 builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
