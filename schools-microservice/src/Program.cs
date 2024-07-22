9a6a310f2d913f2b898d7c11ca79e3ed2a15868a
nelzonWin <nelzonWin@codeW.com>
2024-07-22 15:10:34 -0500

    backend: building Program.cs

--- schools-microservice/src/Program.cs
+++ schools-microservice/src/Program.cs
@@ -1,6 +1,34 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+var builder = WebApplication.CreateBuilder(args);//builder de la app
 
-app.MapGet("/", () => "Hello World!");
+// Add services to the container.
+builder.Services.AddControllers();//controladores para manejar solicitudes HTTP y dar rptas
+
+// servicios para interactuar
+// builder.Services.AddScoped<ISchoolService, SchoolService>();
+// builder.Services.AddScoped<ICourseService, CourseService>();
+// builder.Services.AddScoped<ITeacherService, TeacherService>();
+// builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+
+// // Register repositories
+// builder.Services.AddScoped<ISchoolRepository, SchoolRepository>();
+// builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+// builder.Services.AddScoped<ITeacherRepository, TeacherRepository>();
+// builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+
+var app = builder.Build();//construye nuestra app
+
+/*
+Habilito página de errores de desarrollo que proporciona 
+información detallada sobre cualquier excepción que ocurra.
+*/
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
